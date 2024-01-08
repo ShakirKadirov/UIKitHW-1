ed99f8441ca6bab873ea8ed715bc2bc76851957a
ShakirKadirov <shakirkadirov1@gmail.com>
2024-01-09 01:35:53 +0600

    Задачу сделал но еще буду добавить Lifecycle ViewController

--- UIKitHW-1/ViewController.swift
+++ UIKitHW-1/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myName: UITextField!
+    @IBOutlet weak var myLastName: UITextField!
+    @IBOutlet weak var myGroupID: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        myName.textColor = .white
+        myLastName.textColor = .white
+        myGroupID.textColor = .white
     }
 
+    @IBAction func setName(_ sender: Any) {
+        
+        let textField = "Shakir"
+        myName.text = textField
+//        print(MyName.text)
+        print("set name")
+    }
+    
+    @IBAction func setLastName(_ sender: Any) {
+        let textField = "Kadirov"
+        myLastName.text = textField
+        print("set last name")
 
+    }
+    
+    @IBAction func setGroupID(_ sender: Any) {
+        let textField = "IOS 20.11"
+        myGroupID.text = textField
+        print("set group id")
+    }
+    
 }
 
