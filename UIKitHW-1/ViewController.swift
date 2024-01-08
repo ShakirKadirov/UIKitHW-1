951b3c57e1d351171d9d92b3bbe80c6aab21bffa
ShakirKadirov <shakirkadirov1@gmail.com>
2024-01-09 02:04:31 +0600

    change new color to button with code

--- UIKitHW-1/ViewController.swift
+++ UIKitHW-1/ViewController.swift
@@ -13,33 +13,43 @@ class ViewController: UIViewController {
     @IBOutlet weak var myLastName: UITextField!
     @IBOutlet weak var myGroupID: UITextField!
     
+    
+    @IBOutlet weak var nameButton: UIButton!
+    @IBOutlet weak var lastnameButton: UIButton!
+    @IBOutlet weak var groupIdButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         myName.textColor = .white
         myLastName.textColor = .white
         myGroupID.textColor = .white
+        
+        nameButton.backgroundColor = .brown
+        nameButton.layer.cornerRadius = 10
+        
+        lastnameButton.backgroundColor = .orange
+        lastnameButton.layer.cornerRadius = 10
+        
+        groupIdButton.backgroundColor = .magenta
+        groupIdButton.layer.cornerRadius = 10
     }
 
     @IBAction func setName(_ sender: Any) {
         
         let textField = "Shakir"
         myName.text = textField
-//        print(MyName.text)
         print("set name")
     }
     
     @IBAction func setLastName(_ sender: Any) {
         let textField = "Kadirov"
         myLastName.text = textField
-        print("set last name")
-
     }
     
     @IBAction func setGroupID(_ sender: Any) {
         let textField = "IOS 20.11"
         myGroupID.text = textField
-        print("set group id")
     }
     
 }
